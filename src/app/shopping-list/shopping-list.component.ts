1c3233a8d6f88534537b039808586235da5d66a6
Nadia Vetrano <nadia.vetrano@eltechnology.it>
2018-06-01 17:53:07 +0200

    Routes and Observables added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[];
+export class ShoppingListComponent implements OnInit, OnDestroy {
+    ingredients: Ingredient[];
+    private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService) {
-  }
+    constructor(private slService: ShoppingListService) {
+    }
 
-  ngOnInit() {
-    console.log('ShoppingListComponent: OnInit');
-    this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
-  }
+    ngOnInit() {
+        console.log('ShoppingListComponent: OnInit');
+        this.ingredients = this.slService.getIngredients();
+        this.subscription = this.slService.ingredientsChanged.subscribe(
+            (ingredients: Ingredient[]) => {
+                this.ingredients = ingredients;
+            }
+        );
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,30 +1,32 @@
 import {Ingredient} from '../shared/ingredient.model';
-import {EventEmitter} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
-  private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+    ingredientsChanged = new Subject<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ];
 
-  getIngredients() {
-    return this.ingredients.slice();
-  }
+    getIngredients() {
+        return this.ingredients.slice();
+    }
 
-  addIngredient(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
-    console.log('Ingredient added!!');
-  }
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        // this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
+        console.log('Ingredient added!!');
+    }
 
-  addIngredients(ingredients: Ingredient[]) {
-    // for (let ingredient of ingredients){
-    //   this.addIngredient(ingredient);
-    // }
-    // spread operator
-    this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
-    console.log('Ingredients added');
-  }
+    addIngredients(ingredients: Ingredient[]) {
+        // for (let ingredient of ingredients){
+        //   this.addIngredient(ingredient);
+        // }
+        // spread operator
+        this.ingredients.push(...ingredients);
+        // this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
+        console.log('Ingredients added');
+    }
 }
